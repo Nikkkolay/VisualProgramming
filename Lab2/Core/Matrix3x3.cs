84854c1b11d5f604f0560dda5849f1b35d6d79f1
Nikolay Baturin <nikolay.baturin.998@mail.ru>
2020-03-19 23:35:48 +0400

    add: init commit

---
+++ Lab2/Core/Matrix3x3.cs
@@ -0,0 +1,128 @@
+﻿namespace Lab2.Core
+{
+    public class Matrix3X3
+    {
+        public const int MatrixSize = 3;
+        private readonly double[,] _matrix;
+
+        public Matrix3X3(double[,] matrix)
+        {
+            _matrix = new double[MatrixSize, MatrixSize];
+
+            for (var i = 0; i < MatrixSize; i++)
+            {
+                for (var j = 0; j < MatrixSize; j++)
+                {
+                    _matrix[i, j] = matrix[i, j];
+                }
+            }
+        }
+
+        public double this[int i, int j] => _matrix[i, j];
+
+        public static Matrix3X3 operator +(Matrix3X3 leftMatrix, Matrix3X3 rightMatrix)
+        {
+            return Sum(leftMatrix, rightMatrix);
+        }
+
+        public static Matrix3X3 operator -(Matrix3X3 leftMatrix, Matrix3X3 rightMatrix)
+        {
+            return Sum(leftMatrix, -rightMatrix);
+        }
+
+        public static Matrix3X3 operator -(Matrix3X3 matrix)
+        {
+            var matrixValues = new double[MatrixSize, MatrixSize];
+
+            for (var i = 0; i < MatrixSize; i++)
+            {
+                for (var j = 0; j < MatrixSize; j++)
+                {
+                    matrixValues[i, j] = -matrix[i, j];
+                }
+            }
+
+            return new Matrix3X3(matrixValues);
+        }
+
+        public static Matrix3X3 operator *(Matrix3X3 leftMatrix, Matrix3X3 rightMatrix)
+        {
+            var matrixValues = new double[MatrixSize, MatrixSize];
+
+            for (var i = 0; i < MatrixSize; i++)
+            {
+                for (var j = 0; j < MatrixSize; j++)
+                {
+                    matrixValues[i, j] = 0;
+                    for (var k = 0; k < MatrixSize; k++)
+                    {
+                        matrixValues[i, j] += leftMatrix[i, k] + rightMatrix[k, j];
+                    }
+                }
+            }
+
+            return new Matrix3X3(matrixValues);
+        }
+
+        public double Determinant()
+        {
+            //Calculated by rule of Sarrus.
+            return
+                _matrix[0, 0] * _matrix[1, 1] * _matrix[2, 2] +
+                _matrix[0, 1] * _matrix[1, 2] * _matrix[2, 0] +
+                _matrix[0, 2] * _matrix[1, 0] * _matrix[2, 1] -
+                _matrix[0, 2] * _matrix[1, 1] * _matrix[2, 0] -
+                _matrix[0, 0] * _matrix[1, 2] * _matrix[2, 1] -
+                _matrix[0, 1] * _matrix[1, 0] * _matrix[2, 2];
+        }
+
+        public double SumOfElementsOnMainDiagonal()
+        {
+            double answer = 0;
+
+            for (var i = 0; i < MatrixSize; i++)
+            {
+                answer += _matrix[i, i];
+            }
+
+            return answer;
+        }
+
+        public double SumOfElementsOnSideDiagonal()
+        {
+            double answer = 0;
+
+            for (var i = 0; i < MatrixSize; i++)
+            {
+                answer += _matrix[i, MatrixSize - i - 1];
+            }
+
+            return answer;
+        }
+
+        public override string ToString()
+        {
+            return "[" +
+                   $"\t[{_matrix[0, 0]}, {_matrix[0, 1]}, {_matrix[0, 2]}]\n" +
+                   $"\t[{_matrix[1, 0]}, {_matrix[1, 1]}, {_matrix[1, 2]}]\n" +
+                   $"\t[{_matrix[2, 0]}, {_matrix[2, 1]}, {_matrix[2, 2]}]\n" +
+                   "]";
+        }
+
+
+        private static Matrix3X3 Sum(Matrix3X3 leftMatrix, Matrix3X3 rightMatrix)
+        {
+            var matrixValues = new double[MatrixSize, MatrixSize];
+
+            for (var i = 0; i < MatrixSize; i++)
+            {
+                for (var j = 0; j < MatrixSize; j++)
+                {
+                    matrixValues[i, j] = leftMatrix[i, j] + rightMatrix[i, j];
+                }
+            }
+
+            return new Matrix3X3(matrixValues);
+        }
+    }
+}
---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Lab2.Core;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace lab2_view
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var leftMatrix = new Matrix3X3(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+            WriteMatrix(DataGrid1, leftMatrix);
+            var rightMatrix = new Matrix3X3(new double[,] { { 7, 2, 3 }, { 4, 0, 4 }, { -7, 8, 9 } });
+            WriteMatrix(DataGrid2, rightMatrix);
+
+            var matrixSum = leftMatrix + rightMatrix;
+            WriteMatrix(DataGrid3, matrixSum);
+
+            var matrixSub = leftMatrix - rightMatrix;
+            WriteMatrix(DataGrid4, matrixSub);
+
+            Determinant.Text = leftMatrix.Determinant().ToString();
+            SumOnMainDiagonal.Text = leftMatrix.SumOfElementsOnMainDiagonal().ToString();
+            SumOnSideDiagonal.Text = leftMatrix.SumOfElementsOnSideDiagonal().ToString();
+        }
+
+        private void WriteMatrix(DataGrid view, Matrix3X3 matrix)
+        {
+            var data = ConvertMatrixToListArray(matrix);
+            view.ItemsSource = data;
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                var column = new DataGridTextColumn();
+                column.Binding = new Binding(string.Format("[{0}]", i));
+                view.Columns.Add(column);
+            }
+                
+        }
+
+        private List<double[]> ConvertMatrixToListArray(Matrix3X3 matrix)
+        {
+            var data = new List<double[]>();
+            for (int i = 0; i < 3; i++)
+            {
+                var row = new double[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    row[j] = matrix[i, j];
+                }
+                data.Add(row);
+            }
+            return data;
+        }
+    }
+}
