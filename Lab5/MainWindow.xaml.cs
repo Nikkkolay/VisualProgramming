37ddb5cba9c66c9dbede5be12cf738bc2740bb29
Nikolay Baturin <nikolay.baturin.998@mail.ru>
2020-03-29 10:04:34 +0400

    add: Lab5

---
+++ Lab5/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var element = new YControlPanel();
+            element.YPositionChanged += OnYPositionChanged;
+            MainGrid.Children.Add(element);
+            Grid.SetColumn(element, 0);
+        }
+
+        public void OnYPositionChanged(double y)
+        {
+            Label.Content = y;
+        }
+    }
+}
