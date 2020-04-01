a1ec600510af454ffa9f0ae7cbf9405da7be7a06
Nikolay Baturin <nikolay.baturin.998@mail.ru>
2020-04-01 22:20:20 +0400

    add: Lab8

---
+++ Lab8/Item.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using System.Xml.Linq;
+
+namespace Lab8
+{
+    public partial class Item : UserControl
+    {
+        public Item(XElement item)
+        {
+            InitializeComponent();
+
+            var date = item.Element("pubDate");
+            var titleText = item.Element("title");
+            var titleUri = item.Element("link");
+            var description = item.Element("description");
+
+            ShowDate(date.Value);
+            ShowTitle(titleText.Value, titleUri.Value);
+            if(description != null)
+            {
+                Description.Text = description.Value;
+            }
+        }
+
+        private void ShowDate(string dateString)
+        {
+            string parseFormat = "ddd, dd MMM yyyy HH:mm:ss zzz";
+            DateTime date = DateTime.ParseExact(dateString, parseFormat, CultureInfo.InvariantCulture);
+            Date.Content = date;
+        }
+
+        private void ShowTitle(string title, string uri)
+        {
+            Hyperlink hyperLink = new Hyperlink()
+            {
+                NavigateUri = new Uri(uri)
+            };
+            hyperLink.RequestNavigate += HyperlinkRequestNavigate;
+            hyperLink.Inlines.Add(title);
+            Title.Inlines.Add(hyperLink);
+        }
+
+        private void HyperlinkRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.ToString());
+        }
+    }
+}
---
+++ Lab8/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Lab8
+{
+    public partial class MainWindow : Window
+    {
+        private XElement _data;
+        BackgroundWorker _backgroundWorker;
+        private string _lastUpdateData = "";
+        private bool _isLoading;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _backgroundWorker = new BackgroundWorker();
+            _backgroundWorker.DoWork += LoadData;
+            _backgroundWorker.RunWorkerCompleted += OnRunWorkerCompleted;
+
+            TryUpdateData();
+        }
+
+        private void TryUpdateData()
+        {
+            if (_isLoading) return;
+
+            _isLoading = true;
+            _backgroundWorker.RunWorkerAsync("http://www.consultant.ru/rss/hotdocs.xml");
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            TryUpdateData();
+        }
+
+        private void LoadData(object sender, DoWorkEventArgs e)
+        {
+            _data = XElement.Load(e.Argument.ToString());
+
+            e.Result = _data.Element("channel").Element("lastBuildDate").Value;
+        }
+
+        private void OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            _isLoading = false;
+            var updateTime = (string)e.Result;
+
+            if (_lastUpdateData == "")
+            {
+                _lastUpdateData = updateTime;
+                ParsData();
+                return;
+            }
+            if(_lastUpdateData != updateTime)
+            {
+                _lastUpdateData = updateTime;
+                ParsData();
+                MessageBox.Show("Данные обновленны!");
+            }
+            else
+            {
+                MessageBox.Show("Обновлений не было.");
+            }
+        }
+
+        private void ParsData()
+        {
+            XElement channel = _data.Element("channel");
+
+            NewsTitle.Content = channel.Element("title").Value;
+
+            Description.Content = channel.Element("description").Value;
+
+            string parseFormat = "ddd, dd MMM yyyy HH:mm:ss zzz";
+            DateTime date = DateTime.ParseExact(channel.Element("lastBuildDate").Value, parseFormat, CultureInfo.InvariantCulture);
+            Date.Content = date;
+
+            foreach (var element in channel.Elements("item"))
+            {
+                var item = new Item(element);
+                item.Margin = new Thickness(5);
+                PlaseForItems.Children.Add(item);
+            }
+        }
+    }
+}
